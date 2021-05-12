ed42e78956d217ed74ac4640aea2b471118f0790
PraVEeen-chouhan <84060969+PraVEeen-chouhan@users.noreply.github.com>
2021-05-12 11:55:56 +0530

    Create for doing swapping number.c

---
+++ for doing swapping number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c;
+  a=30;
+  b=90;
+  printf("Value of a is %d\n Value of b is %d",a,b);
+  //veriable of c is temporary veriable
+  //value of a assgin to c whish is temporary veriable
+  c=a;
+  //value of b assgin to a
+  a=b;
+  //value of c assgin to b
+  b=c;
+  printf("afterr swapping value of a is %d \n",a);
+  printf("after swapping value of b is %d",b);
+  return 0;
+}
