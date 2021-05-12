78c0d3650764323128371f82b6279c830750f3cc
PraVEeen-chouhan <84060969+PraVEeen-chouhan@users.noreply.github.com>
2021-05-12 13:41:11 +0530

    Update for using const. code.c

--- for using const. code.c
+++ for using const. code.c
@@ -3,10 +3,9 @@ int main()
 {
   const int a=24;
   int b;
-  a=25;
-  //but a is a constnt so print only 24 not a new valye like 25
+  //in this program ;a is aconsten veriable
   b=78;
-  printf("%d , %d",a,b);
+  printf("%d , %d \n",a,b);
   b=89;
   printf("%d ,%d",a,b);
   return 0;
