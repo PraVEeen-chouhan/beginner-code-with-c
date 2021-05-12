7bf108e7b1452c93f20fd2dcace046fd8fb6227d
PraVEeen-chouhan <84060969+PraVEeen-chouhan@users.noreply.github.com>
2021-05-12 11:07:32 +0530

    Create for using addition of two veriable.c

---
+++ for using addition of two veriable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  int b;
+  int c;
+  int d;
+  int e;
+  a=77;
+  b=27;
+  c=57;
+  d=67;
+  e=a+b=c=d;
+  prinf("sum of foue number is %d",e);
+  return 0;
+}
