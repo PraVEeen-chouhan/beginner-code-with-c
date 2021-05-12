ff6781236a5d80d5d9faf1e8de579dce47f96b42
PraVEeen-chouhan <84060969+PraVEeen-chouhan@users.noreply.github.com>
2021-05-12 11:43:01 +0530

    Create for take input and give squre of number output.c

---
+++ for take input and give squre of number output.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  int b;
+  printf("enter the number which you find a squre=");
+  scaanf("%d",&a);
+  b=a*a;
+  printf("squre of this number = %d")
+  return 0;
+}
+  
