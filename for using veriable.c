bcef1d2a40193dd34f7b27aa3a7e396b2bfbd664
PraVEeen-chouhan <84060969+PraVEeen-chouhan@users.noreply.github.com>
2021-05-12 10:49:06 +0530

    Create for using veriable.c

---
+++ for using veriable.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  //for useing verable
+  int a;
+  int b;
+  a=7;
+  b=80;
+  printf("value of first veriable is %d",a);
+  printf("value of second veriable in %d",b);
+  rturn 0;
+  
+}
