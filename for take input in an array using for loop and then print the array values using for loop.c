f110adcb17a0f298a069c6ee3a2d9aa93cc9dd86
PraVEeen-chouhan <84060969+PraVEeen-chouhan@users.noreply.github.com>
2021-07-28 15:33:46 +0530

    Create for take input in an array using for loop and then print the array values using for loop.c

---
+++ for take input in an array using for loop and then print the array values using for loop.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+int main()
+{
+  int i;
+	int num[]={5,9,6,3,7,4};
+	for(i=0; i<=5; i++)
+	{
+	  printf("\nThe numbers are %d \n",num[i]);	
+	}
+ int j; 
+ int nu[5];
+ for(j=0; j<=5; j++)
+ { 
+   
+   scanf(" %d \n",&nu[j]);
+ }
+	return 0;
+}
