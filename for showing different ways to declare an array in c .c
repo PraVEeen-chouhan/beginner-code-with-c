346ff308c58f92b84200faf922dc480bbf95b259
PraVEeen-chouhan <84060969+PraVEeen-chouhan@users.noreply.github.com>
2021-07-28 15:32:25 +0530

    Create for showing different ways to declare an array in c .c

---
+++ for showing different ways to declare an array in c .c
@@ -0,0 +1,12 @@
+include<stdio.h>
+int main()
+{
+
+ int m[5]={5,8,2,0,1};
+ printf("The element are %d \n",m[0]);
+ printf("The element are %d \n",m[1]);
+ printf("The element are %d \n",m[2]);
+ printf("The elementare %d \n",m[3]);
+ printf("The element are %d \n",m[4]);
+  return 0;
+}
