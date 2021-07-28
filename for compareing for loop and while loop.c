0d70844b62acc6a2fdc99e5a0b6f6ef51c2bbe53
PraVEeen-chouhan <84060969+PraVEeen-chouhan@users.noreply.github.com>
2021-07-28 15:36:43 +0530

    Create for compareing for loop and while loop.c

---
+++ for compareing for loop and while loop.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+  
+ int j;
+ 
+ printf("In for loop");
+ for(j=0; j<=7; j++)
+ {
+   printf("The value is %d \n",j);
+ }
+ 
+ printf("In while loop\n");
+ int k=0;
+ while(k<=5)
+ {
+   printf(" %d \n",k);
+   k=k+1;
+ }
+	return 0;
+}
